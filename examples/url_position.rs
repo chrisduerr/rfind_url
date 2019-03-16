4544cc2930594f0bafe7c4b91a0757dd4bd8e990
Christian Duerr <contact@christianduerr.com>
2019-03-17 00:49:55 +0100

    Add example program

---
+++ examples/url_position.rs
@@ -0,0 +1,24 @@
+use rfind_url::Parser;
+
+fn main() {
+    let input = "before https://example.org after";
+
+    let mut parser = Parser::new();
+    for (i, c) in input.chars().rev().enumerate() {
+        if let Some(url_len) = parser.advance(c) {
+            let url_start = input.len() - i - 1;
+            let url_end = url_start + url_len as usize - 1;
+
+            println!(
+                "Input '{}' contains a URL starting at position '{}' going until position '{}'",
+                input, url_start, url_end
+            );
+
+            assert_eq!(url_len, 19);
+            assert_eq!(i, 24);
+
+            break;
+        }
+    }
+}
+
